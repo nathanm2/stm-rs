a447b5ff018cb323c7d755d19401a3a642534140
Nathan Miller <Nathan.Miller@wdc.com>
2019-03-12 17:39:38 -0500

    Adding closures and error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,51 @@
+use std::result;
+
 pub struct FrameDecoder {
     stream: Option<u8>,
 }
+
+#[derive(Debug)]
+pub enum FrameDecoderError {
+    InvalidStreamId,
+    InvalidAuxByte,
+}
+
+use self::FrameDecoderError::*;
+
+pub type Result<T> = result::Result<T, FrameDecoderError>;
 
 impl FrameDecoder {
     pub fn new() -> Self {
         FrameDecoder { stream: None }
     }
 
-    pub fn decode_frame(&mut self, frame: &[u8; 16]) {
+    pub fn decode_frame<B>(&mut self, frame: &[u8; 16], out: B)
+    where
+        B: Fn(Option<u8>, u8),
+    {
         let mut cur_stream = self.stream;
         let mut next_stream = None;
         let aux_byte = frame[15];
 
         for (i, byte) in frame[..15].iter().enumerate() {
-            if i % 2 == 0 {  // Even: data or ID change.
-                let aux_bit = (aux_byte >> (i/2)) & 0x01;
-                if byte & 0x01 == 1 { // Id Change.
-                    if aux_bit == 1 { // Delayed ID Change.
+            // Even byte: ID change OR data.
+            if i % 2 == 0 {
+                let aux_bit = (aux_byte >> (i / 2)) & 0x01;
+                // Id Change.
+                if byte & 0x01 == 1 {
+                    if aux_bit == 1 {
+                        // Delayed ID Change.
                         next_stream = Some(byte >> 1);
-                    } else { // Immediate ID change.
+                    } else {
+                        // Immediate ID change.
                         cur_stream = Some(byte >> 1);
                     }
-                } else { // data
-                    println!("{}={}", i, byte | aux_bit);
+                } else {
+                    out(cur_stream, byte | aux_bit);
                 }
-            } else { // Odd : data only.
-                println!("{}={}", i, byte);
+            } else {
+                // Odd byte: Data only.
+                out(cur_stream, *byte);
                 if let Some(_) = next_stream {
                     cur_stream = next_stream;
                     next_stream = None;
@@ -33,5 +53,48 @@ impl FrameDecoder {
             }
         }
         self.stream = cur_stream;
+    }
+
+    pub fn decode_frame_safe<B>(&mut self, frame: &[u8; 16], out: B) -> Result<()>
+    where
+        B: Fn(Option<u8>, u8),
+    {
+        let mut cur_stream = self.stream;
+        let mut next_stream = None;
+        let aux_byte = frame[15];
+
+        for (i, byte) in frame[..15].iter().enumerate() {
+            if i % 2 == 0 {
+                // Even byte: ID change OR data.
+                let aux_bit = (aux_byte >> (i / 2)) & 0x01;
+                if byte & 0x01 == 1 {
+                    if *byte == 0xFF {
+                        return Err(InvalidStreamId);
+                    }
+                    // Id Change.
+                    if aux_bit == 1 {
+                        if i == 14 {
+                            return Err(InvalidAuxByte);
+                        }
+                        // Delayed ID Change.
+                        next_stream = Some(byte >> 1);
+                    } else {
+                        // Immediate ID change.
+                        cur_stream = Some(byte >> 1);
+                    }
+                } else {
+                    out(cur_stream, byte | aux_bit);
+                }
+            } else {
+                // Odd byte: Data only.
+                out(cur_stream, *byte);
+                if let Some(_) = next_stream {
+                    cur_stream = next_stream;
+                    next_stream = None;
+                }
+            }
+        }
+        self.stream = cur_stream;
+        Ok(())
     }
 }
--- tests/frame_decode.rs
+++ tests/frame_decode.rs
@@ -1,6 +1,32 @@
+use std::error;
+
+fn display(stream: Option<u8>, byte: u8) {
+    match stream {
+        Some(id) => println!("{} = {}", id, byte),
+        None => println!("<Unknown> = {}", byte),
+    }
+}
+
 #[test]
-fn it_works() {
+fn good_path() {
     let mut d = stm::FrameDecoder::new();
-    let frame = [0; 16];
-    d.decode_frame(&frame)
+    let mut frame = [0; 16];
+
+    frame[0] = 0x03;
+    frame[2] = 0x05;
+    d.decode_frame(&frame, display)
+}
+
+#[test]
+fn bad_path() {
+    let mut d = stm::FrameDecoder::new();
+    let mut frame = [0; 16];
+
+    frame[0] = 0x03;
+    frame[2] = 0xFF;
+    let r = d.decode_frame_safe(&frame, display);
+    match r {
+        Ok(_) => println!("Everything works"),
+        Err(code) => println!("Failure encountered: {:?}", code),
+    }
 }
