82e6c67de760573cfd03cdda24aa52c4804d3a77
Nathan Miller <Nathan.Miller@wdc.com>
2019-03-19 17:17:30 -0500

    Decode multiple frames

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,9 @@ pub struct FrameDecoder {
 
 #[derive(Debug)]
 pub enum FrameDecoderError {
-    InvalidStreamId,
-    InvalidAuxByte,
+    InvalidStreamId(usize),
+    InvalidAuxByte(usize),
+    PartialFrame(usize),
 }
 
 use self::FrameDecoderError::*;
@@ -19,82 +20,52 @@ impl FrameDecoder {
         FrameDecoder { stream: None }
     }
 
-    pub fn decode_frame<B>(&mut self, frame: &[u8; 16], out: B)
+    pub fn decode<H>(&mut self, frames: &[u8], handler: H) -> Result<()>
     where
-        B: Fn(Option<u8>, u8),
+        H: Fn(Option<u8>, u8),
     {
         let mut cur_stream = self.stream;
-        let mut next_stream = None;
-        let aux_byte = frame[15];
 
-        for (i, byte) in frame[..15].iter().enumerate() {
-            // Even byte: ID change OR data.
-            if i % 2 == 0 {
-                let aux_bit = (aux_byte >> (i / 2)) & 0x01;
-                // Id Change.
-                if byte & 0x01 == 1 {
-                    if aux_bit == 1 {
-                        // Delayed ID Change.
-                        next_stream = Some(byte >> 1);
-                    } else {
-                        // Immediate ID change.
-                        cur_stream = Some(byte >> 1);
-                    }
-                } else {
-                    out(cur_stream, byte | aux_bit);
-                }
-            } else {
-                // Odd byte: Data only.
-                out(cur_stream, *byte);
-                if let Some(_) = next_stream {
-                    cur_stream = next_stream;
-                    next_stream = None;
-                }
+        for (count, frame) in frames.chunks(16).enumerate() {
+            if frame.len() < 16 {
+                return Err(PartialFrame(count * 16));
             }
-        }
-        self.stream = cur_stream;
-    }
 
-    pub fn decode_frame_safe<B>(&mut self, frame: &[u8; 16], out: B) -> Result<()>
-    where
-        B: Fn(Option<u8>, u8),
-    {
-        let mut cur_stream = self.stream;
-        let mut next_stream = None;
-        let aux_byte = frame[15];
-
-        for (i, byte) in frame[..15].iter().enumerate() {
-            if i % 2 == 0 {
-                // Even byte: ID change OR data.
-                let aux_bit = (aux_byte >> (i / 2)) & 0x01;
-                if byte & 0x01 == 1 {
-                    if *byte == 0xFF {
-                        return Err(InvalidStreamId);
-                    }
-                    // Id Change.
-                    if aux_bit == 1 {
-                        if i == 14 {
-                            return Err(InvalidAuxByte);
+            let mut next_stream = None;
+            let aux_byte = frame[15];
+            for (i, byte) in frame[..15].iter().enumerate() {
+                if i % 2 == 0 {
+                    // Even byte: ID change OR data.
+                    let aux_bit = (aux_byte >> (i / 2)) & 0x01;
+                    if byte & 0x01 == 1 {
+                        if *byte == 0xFF {
+                            return Err(InvalidStreamId(count * 16 + i));
                         }
-                        // Delayed ID Change.
-                        next_stream = Some(byte >> 1);
+                        // Id Change.
+                        if aux_bit == 1 {
+                            if i == 14 {
+                                return Err(InvalidAuxByte(count * 16 + i));
+                            }
+                            // Delayed ID Change.
+                            next_stream = Some(byte >> 1);
+                        } else {
+                            // Immediate ID change.
+                            cur_stream = Some(byte >> 1);
+                        }
                     } else {
-                        // Immediate ID change.
-                        cur_stream = Some(byte >> 1);
+                        handler(cur_stream, byte | aux_bit);
                     }
                 } else {
-                    out(cur_stream, byte | aux_bit);
-                }
-            } else {
-                // Odd byte: Data only.
-                out(cur_stream, *byte);
-                if let Some(_) = next_stream {
-                    cur_stream = next_stream;
-                    next_stream = None;
+                    // Odd byte: Data only.
+                    handler(cur_stream, *byte);
+                    if let Some(_) = next_stream {
+                        cur_stream = next_stream;
+                        next_stream = None;
+                    }
                 }
             }
         }
         self.stream = cur_stream;
-        Ok(())
+        return Ok(());
     }
 }
--- tests/frame_decode.rs
+++ tests/frame_decode.rs
@@ -1,5 +1,3 @@
-use std::error;
-
 fn display(stream: Option<u8>, byte: u8) {
     match stream {
         Some(id) => println!("{} = {}", id, byte),
@@ -10,21 +8,21 @@ fn display(stream: Option<u8>, byte: u8) {
 #[test]
 fn good_path() {
     let mut d = stm::FrameDecoder::new();
-    let mut frame = [0; 16];
+    let mut frame = [0; 32];
 
     frame[0] = 0x03;
     frame[2] = 0x05;
-    d.decode_frame(&frame, display)
+    d.decode(&frame, display).expect("Not Ok");
 }
 
 #[test]
 fn bad_path() {
     let mut d = stm::FrameDecoder::new();
-    let mut frame = [0; 16];
+    let mut frame = [0; 32];
 
     frame[0] = 0x03;
     frame[2] = 0xFF;
-    let r = d.decode_frame_safe(&frame, display);
+    let r = d.decode(&frame, display);
     match r {
         Ok(_) => println!("Everything works"),
         Err(code) => println!("Failure encountered: {:?}", code),
