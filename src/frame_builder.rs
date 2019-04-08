ee55be927126074d7f758e2792dbcb1542256d07
Nathan Miller <Nathan.Miller@wdc.com>
2019-04-08 07:54:19 -0500

    Towards a standalone FrameBuilder

--- src/frame_builder.rs
+++ src/frame_builder.rs
@@ -3,6 +3,7 @@ use std::result;
 #[derive(Debug, PartialEq)]
 pub enum FrameBuilderError {
     InvalidOffset(usize),
+    InvalidStreamId(u8),
 }
 
 use FrameBuilderError::*;
@@ -12,6 +13,10 @@ pub type Result = result::Result<(), FrameBuilderError>;
 pub fn set_stream_id(frames: &mut [u8], offset: usize, id: u8, immediate: bool) -> Result {
     if offset % 2 != 0 || offset % 16 == 15 {
         return Err(InvalidOffset(offset));
+    }
+
+    if id >= 0xfe {
+        return Err(InvalidStreamId(id));
     }
 
     let aux_offset = (offset / 16) + 15;
@@ -52,39 +57,59 @@ pub fn set_stream_data(frames: &mut [u8], offset: usize, data: u8) -> Result {
 pub struct FrameBuilder {
     frames: Vec<u8>,
     offset: usize,
+    last_data: Option<u8>, // Last byte of data written.
 }
 
 impl FrameBuilder {
-    pub fn new(frame_count: usize) -> FrameBuilder {
+    pub fn new(capacity: usize) -> FrameBuilder {
         FrameBuilder {
-            frames: vec![0; frame_count * 16],
+            frames: Vec<u8>::with_capacity(capacity),
             offset: 0,
+            last_data: None
         }
     }
 
+    // See if we need to add a new frame:
     fn check(&mut self) {
         if self.offset == self.frames.len() {
             self.frames.resize(self.frames.len() + 16, 0);
         }
     }
 
+    // Skip over the aux byte:
     fn increment_offset(&mut self) {
         self.offset += if self.offset % 16 == 15 { 2 } else { 1 };
     }
 
-    fn set_id(&mut self, value: u8, immediate: bool) {
+    fn set_id_direct(&mut self, value: u8, immediate: bool) -> Result {
         self.check();
-        set_stream_id(&mut self.frames, self.offset, value, immediate).unwrap();
+        set_stream_id(&mut self.frames, self.offset, value, immediate)?;
         self.increment_offset();
+        Ok(())
+    }
+
+    fn set_id(&mut self, value: u8) -> Result {
+        if self.offset % 2 == 1 {
+            self.offset -= 1;
+            match self.last_data {
+                None => self.set_id_full(value, true)?,
+                Some(byte) => {self.set_id_full(value, false)?;
+                    self.set_data(byte)?; }
+            }
+        } else {
+            self.set_id_full(value, true)?;
+            self.last_data = None;
+        }
+        Ok(())
     }
 
     pub fn immediate_id(mut self, value: u8) -> FrameBuilder {
-        self.set_id(value, true);
+        self.set_id_full(value, true).unwrap();
         self
     }
 
     pub fn delayed_id(mut self, value: u8) -> FrameBuilder {
-        self.set_id(value, false);
+        self.set_id_full(value, false).unwrap();
         self
     }
 
@@ -92,6 +117,8 @@ impl FrameBuilder {
         self.check();
         set_stream_data(&mut self.frames, self.offset, value).unwrap();
         self.increment_offset();
+        self.last_data = Some(value);
+
     }
 
     pub fn data_span<F>(mut self, span: usize, mut f: F) -> FrameBuilder
