4f190d358078e43241cd334c3c83134c8e4db711
Nathan Miller <Nathan.Miller@wdc.com>
2019-04-16 00:17:56 -0500

    Initial IO Support

--- src/bin/nibbles.rs
+++ src/bin/nibbles.rs
@@ -1,16 +1,20 @@
 use std::error::Error;
 use std::fmt;
+use std::fs::File;
+use std::io::{ErrorKind, Read};
 use std::path::PathBuf;
 
 #[derive(Debug)]
-enum NibblesError {
+enum CliError {
     UsageError(String),
     OtherError(String),
 }
 
-impl Error for NibblesError {}
+use CliError::*;
 
-impl fmt::Display for NibblesError {
+impl Error for CliError {}
+
+impl fmt::Display for CliError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             UsageError(msg) | OtherError(msg) => write!(f, "{}", msg),
@@ -18,23 +22,51 @@ impl fmt::Display for NibblesError {
     }
 }
 
-use NibblesError::*;
+type Result<T> = std::result::Result<T, CliError>;
 
-fn display_usage() {
-    println!("usage: nibbles [OPTIONS] [FILE ...]");
+impl std::convert::From<std::io::Error> for CliError {
+    fn from(err: std::io::Error) -> CliError {
+        OtherError(format!("{}", err))
+    }
 }
 
-fn run() -> Result<(), NibblesError> {
-    let files: Vec<PathBuf> = std::env::args().skip(1).map(PathBuf::from).collect();
+const DEFAULT_BUF_SIZE: usize = 4 * 1024;
 
-    if files.is_empty() {
-        return Err(OtherError(format!("No files specified")))?;
+fn process_stream<R: ?Sized>(reader: &mut R) -> Result<()>
+where
+    R: Read,
+{
+    let mut buf = [0; DEFAULT_BUF_SIZE];
+    loop {
+        let _len = match reader.read(&mut buf) {
+            Ok(0) => return Ok(()),
+            Ok(len) => len,
+            Err(ref e) if e.kind() == ErrorKind::Interrupted => continue,
+            Err(e) => return Err(CliError::from(e)),
+        };
+    }
+}
+
+fn run() -> Result<()> {
+    let paths: Vec<PathBuf> = std::env::args().skip(1).map(PathBuf::from).collect();
+
+    if paths.is_empty() {
+        return Err(UsageError(format!("No files specified")))?;
+    }
+
+    for path in paths {
+        let mut f = File::open(path)?;
+        process_stream(&mut f)?;
     }
 
     Ok(())
 }
 
 const PROG_NAME: &str = "nibbles";
+
+fn display_usage() {
+    println!("usage: {} [OPTIONS] [FILE ...]", PROG_NAME);
+}
 
 fn main() {
     if let Err(err) = run() {
