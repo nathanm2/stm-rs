2264a1be8391d613c67b9e9eadd42279cf7b9081
Nathan Miller <Nathan.Miller@wdc.com>
2019-05-10 08:24:10 -0500

    WIP: StpDecoder

--- stm-core/src/stp_decoder.rs
+++ stm-core/src/stp_decoder.rs
@@ -1,13 +1,31 @@
-
-pub struct StpParser {
-    synced: bool,
+pub struct StpPacket {
+    pub nibble_offset: u64,
 }
 
-impl StpParser {
-    /// Create a new StpParser.
+pub struct StpDecoder {
+    nibble_offset: u64,
+}
+
+impl StpDecoder {
+    /// Create a new StpDecoder.
     pub fn new() -> Self {
-        StpParser {
-            synced = false,
+        StpDecoder { nibble_offset: 0 }
+    }
+
+    pub fn decode<F>(&mut self, bytes: &[u8], packet_handler: F)
+    where
+        F: Fn(StpPacket),
+    {
+        for byte in bytes {
+            self.decode_nibble(byte & 0xF, packet_handler);
+            self.decode_nibble(byte >> 4, packet_handler);
         }
+    }
+
+    fn decode_nibble<F>(&mut self, nibble: u8, packet_handler: F)
+    where
+        F: Fn(StpPacket),
+    {
+
     }
 }
