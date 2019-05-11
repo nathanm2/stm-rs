192ead2cf8f9db2f0f051be1fb0351f05b306600
Nathan Miller <Nathan.Miller@wdc.com>
2019-05-10 19:04:23 -0500

    WIP: Additional work on StpDecoder

--- stm-core/src/stp_decoder.rs
+++ stm-core/src/stp_decoder.rs
@@ -1,31 +1,95 @@
-pub struct StpPacket {
+pub struct Packet {
     pub nibble_offset: u64,
 }
 
+enum DecoderState {
+    Unsynced,
+    OpCode,
+    Payload,
+}
+
+use self::DecoderState::*;
+
 pub struct StpDecoder {
     nibble_offset: u64,
+    f_count: u8,
+    state: DecoderState,
+    packet_start: u64,
 }
 
 impl StpDecoder {
     /// Create a new StpDecoder.
     pub fn new() -> Self {
-        StpDecoder { nibble_offset: 0 }
-    }
-
-    pub fn decode<F>(&mut self, bytes: &[u8], packet_handler: F)
-    where
-        F: Fn(StpPacket),
-    {
-        for byte in bytes {
-            self.decode_nibble(byte & 0xF, packet_handler);
-            self.decode_nibble(byte >> 4, packet_handler);
+        StpDecoder {
+            nibble_offset: 0,
+            f_count: 0,
+            state: Unsynced,
+            packet_start: 0,
         }
     }
 
-    fn decode_nibble<F>(&mut self, nibble: u8, packet_handler: F)
+    pub fn decode<F>(&mut self, bytes: &[u8], mut handler: F)
     where
-        F: Fn(StpPacket),
+        F: FnMut(Packet),
     {
+        for byte in bytes {
+            self.decode_nibble(byte & 0xF, &mut handler);
+            self.decode_nibble(byte >> 4, &mut handler);
+        }
+    }
 
+    fn decode_nibble<F>(&mut self, nibble: u8, handler: &mut F)
+    where
+        F: FnMut(Packet),
+    {
+        // An ASYNC can happen anywhere within the stream, so we test for that first:
+        self.async_filter(nibble, handler);
+        self.nibble_offset += 1;
+    }
+
+    fn async_filter<F>(&mut self, nibble: u8, handler: &mut F)
+    where
+        F: FnMut(Packet),
+    {
+        if nibble == 0xf {
+            if self.f_count < 21 {
+                self.f_count += 1;
+            } else {
+                self.process(0xf, handler);
+            }
+        } else {
+            if nibble == 0 && self.f_count == 21 {
+                self.process_async(handler);
+            } else {
+                for _ in 0..self.f_count {
+                    self.process(0xf, handler);
+                }
+                self.process(nibble, handler);
+            }
+            self.f_count = 0;
+        }
+    }
+
+    fn process_async<F>(&mut self, _handler: &mut F)
+    where
+        F: FnMut(Packet),
+    {
+        self.to_opcode();
+    }
+
+    fn process<F>(&mut self, _nibble: u8, _handler: &mut F)
+    where
+        F: FnMut(Packet),
+    {
+        match self.state {
+            Unsynced => (),
+            OpCode => (),
+            Payload => (),
+        }
+    }
+
+    fn to_opcode(&mut self) {
+        self.state = OpCode;
+        self.packet_start = self.nibble_offset;
     }
 }
