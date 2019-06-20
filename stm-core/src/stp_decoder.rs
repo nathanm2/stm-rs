0a87fbba104f28a97cd906ceb4e83671ad1e1705
Nathan Miller <Nathan.Miller@wdc.com>
2019-06-19 23:51:28 -0500

    tweaks

--- stm-core/src/stp_decoder.rs
+++ stm-core/src/stp_decoder.rs
@@ -3,15 +3,23 @@ use std::result;
 
 #[derive(Debug, PartialEq)]
 pub enum Error {
-    InvalidAsync { nibble_offset: u64, terminal: u8 },
+    InvalidAsync {
+        nibble_offset: u64,
+        invalid_nibble: u8,
+    },
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            InvalidAsync { nibble_offset, .. } => {
-                write!(f, "invalid async packet at nibble offset {}", nibble_offset)
-            }
+            InvalidAsync {
+                nibble_offset,
+                invalid_nibble,
+            } => write!(
+                f,
+                "Invalid async packet. Nibble offset: {}, Nibble: {}",
+                nibble_offset, invalid_nibble
+            ),
         }
     }
 }
@@ -20,6 +28,12 @@ use self::Error::*;
 
 pub struct Packet {
     pub offset: u64,
+    pub lenth: u64,
+    pub ptype: PacketType,
+}
+
+pub enum PacketType {
+    Async,
 }
 
 pub type Result = result::Result<Packet, Error>;
@@ -32,14 +46,14 @@ enum DecoderState {
 
 use self::DecoderState::*;
 
-const ASYNC_F_COUNT: u8 = 21;
-
 pub struct StpDecoder {
-    nibble_offset: u64, // Offset in nibbles.
-    f_count: u8,        // Number of consecutive 0xF nibbles.
-    state: DecoderState,
+    nibble_offset: u64,  // Offset in nibbles.
+    f_count: u8,         // Number of consecutive 0xF nibbles.
+    state: DecoderState, // The state of the decoder.
     packet_start: u64,
 }
+
+const ASYNC_F_COUNT: u8 = 21;
 
 impl StpDecoder {
     /// Create a new StpDecoder.
@@ -52,6 +66,7 @@ impl StpDecoder {
         }
     }
 
+    /// Decode a stream of bytes.
     pub fn decode_bytes<F>(&mut self, bytes: &[u8], mut handler: F)
     where
         F: FnMut(Result),
@@ -71,49 +86,46 @@ impl StpDecoder {
         }
     }
 
-    fn decode_nibble<F>(&mut self, nibble: u8, mut handler: F)
-    where
-        F: FnMut(Result),
-    {
-        // An ASYNC can appear anywhere within the stream, so we need to check for it first:
-        self.async_filter(nibble, &mut handler);
-        self.nibble_offset += 1;
-    }
-
-    fn async_filter<F>(&mut self, nibble: u8, mut handler: F)
-    where
-        F: FnMut(Result),
-    {
+    fn decode_nibble(&mut self, nibble: u8, handler: &mut dyn FnMut(Result)) {
+        // An ASYNC can appear anywhere within the stream, so every 0xf nibble needs to be buffered
+        // just in case it's part of one.
         if nibble == 0xf {
             if self.f_count < ASYNC_F_COUNT {
                 self.f_count += 1;
             } else {
-                self.process(0xf, &mut handler);
+                self.process(0xf, handler);
             }
         } else {
-            if nibble == 0 && self.f_count == ASYNC_F_COUNT {
-                self.process_async(&mut handler);
+            if self.f_count == ASYNC_F_COUNT {
+                if nibble == 0 {
+                    self.process_async(handler);
+                } else {
+                    self.process_invalid_async(nibble, handler);
+                }
             } else {
                 for _ in 0..self.f_count {
-                    self.process(0xf, &mut handler);
+                    self.process(0xf, handler);
                 }
-                self.process(nibble, &mut handler);
+                self.process(nibble, handler);
             }
             self.f_count = 0;
         }
+        self.nibble_offset += 1;
     }
 
-    fn process_async<F>(&mut self, mut _handler: F)
-    where
-        F: FnMut(Result),
-    {
+    fn process_async(&mut self, _handler: &mut dyn FnMut(Result)) {
         self.state = OpCode;
     }
 
-    fn process<F>(&mut self, _nibble: u8, mut _handler: F)
-    where
-        F: FnMut(Result),
-    {
+    fn process_invalid_async(&mut self, nibble: u8, handler: &mut dyn FnMut(Result)) {
+        self.state = Unsynced;
+        handler(Err(InvalidAsync {
+            nibble_offset: self.nibble_offset - ASYNC_F_COUNT as u64,
+            invalid_nibble: nibble,
+        }));
+    }
+
+    fn process(&mut self, _nibble: u8, _handler: &mut dyn FnMut(Result)) {
         match self.state {
             Unsynced => (),
             OpCode => (),
