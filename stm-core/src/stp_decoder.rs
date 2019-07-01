3105d30dc39a411ed4f58fb75caa0167560156e7
Nathan Miller <Nathan.Miller@wdc.com>
2019-07-01 14:07:44 -0500

    More work

--- stm-core/src/stp_decoder.rs
+++ stm-core/src/stp_decoder.rs
@@ -1,10 +1,10 @@
 use std::fmt;
 use std::result;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     InvalidAsync { start: u64, value: u8 },
-    TruncatedPacket { start: u64, span: u64 },
+    TruncatedPacket { start: u64, span: u8 },
 }
 
 use self::Error::*;
@@ -24,21 +24,36 @@ impl fmt::Display for Error {
     }
 }
 
+#[allow(non_camel_case_types)]
 pub enum OpCode {
-    NULLOpCode = 0x0,
+    NULL = 0x0,
     M8 = 0x1,
     MERR = 0x2,
     C8 = 0x3,
+    D8 = 0x4,
+    D16 = 0x5,
+    D32 = 0x6,
+    D64 = 0x7,
+    D8MTS = 0x8,
+    D16MTS = 0x09,
+    D32MTS = 0x0A,
+    D64MTS = 0x0B,
+    D4 = 0x0C,
+    D4MTS = 0x0D,
+    FLAG_TS = 0x0E,
+    VERSION = 0xF00,
 }
 
 use self::OpCode::*;
 
+#[derive(Debug, PartialEq)]
 pub struct Packet {
     pub start: u64, // Starting offset in nibbles.
-    pub span: u64,
+    pub span: u8,
     pub details: PacketDetails,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum PacketDetails {
     Async,
 }
@@ -51,6 +66,7 @@ enum DecoderState {
     Unsynced,
     OpCode,
     Payload,
+    Timestamp,
 }
 
 use self::DecoderState::*;
@@ -59,7 +75,14 @@ pub struct StpDecoder {
     offset: u64,         // Offset in nibbles.
     f_count: u8,         // Number of consecutive 0xF nibbles.
     state: DecoderState, // The state of the decoder.
-    span: u64,           // Current packet span in nibbles.
+
+    /** Current Packet: **/
+    span: u8, // Current packet span in nibbles.
+    op_code: Option<OpCode>, // Current packet OpCode.
+    payload_sz: u8,          // Current packet payload size (nibbles).
+    has_timestamp: bool,     // Current packet has a timestamp.
+    payload: u64,            // Current packet payload.
+    timestamp: u64,          // Current packet timestamp.
 }
 
 const ASYNC_F_COUNT: u8 = 21;
@@ -72,6 +95,11 @@ impl StpDecoder {
             f_count: 0,
             state: Unsynced,
             span: 0,
+            op_code: None,
+            payload_sz: 0,
+            payload: 0,
+            has_timestamp: false,
+            timestamp: 0,
         }
     }
 
@@ -122,28 +150,11 @@ impl StpDecoder {
         self.offset += 1;
     }
 
-    fn to_state(&mut self, new_state: DecoderState) {
-        match new_state {
-            Unsynced | OpCode => self.span = 0,
-            Payload => (),
-        }
-        self.state = new_state;
-    }
-
-    fn report_truncation(&mut self, handler: &mut dyn FnMut(Result)) {
-        if self.span > 0 {
-            handler(Err(TruncatedPacket {
-                start: self.offset - self.span,
-                span: self.span,
-            }));
-        }
-    }
-
     fn process_async(&mut self, handler: &mut dyn FnMut(Result)) {
         self.report_truncation(handler);
         handler(Ok(Packet {
             start: self.offset - ASYNC_F_COUNT as u64,
-            span: (ASYNC_F_COUNT + 1) as u64,
+            span: ASYNC_F_COUNT + 1,
             details: Async,
         }));
         self.to_state(OpCode);
@@ -158,22 +169,61 @@ impl StpDecoder {
         self.to_state(Unsynced);
     }
 
-    fn process(&mut self, nibble: u8, handler: &mut dyn FnMut(Result)) {
-        match self.state {
-            Unsynced => (),
-            OpCode => self.process_opcode(nibble, handler),
-            Payload => (),
+    fn to_state(&mut self, new_state: DecoderState) {
+        match new_state {
+            Unsynced | OpCode => self.span = 0,
+            Payload => self.payload = 0,
+            Timestamp => self.timestamp = 0,
+        }
+        self.state = new_state;
+    }
+
+    fn report_truncation(&mut self, handler: &mut dyn FnMut(Result)) {
+        if self.span > 0 {
+            handler(Err(TruncatedPacket {
+                start: self.offset - self.span as u64,
+                span: self.span,
+            }));
         }
     }
 
-    fn process_opcode(&mut self, nibble: u8, handler: &mut dyn FnMut(Result)) {
-        match self.span {
-            0 => match nibble {
-                0 => return;  // NULL packet.
-                1 => self.packet_setup(M8, 2, false);
-                2 => self.packet_setup(MERR, 2, false);
-                3 => self.packet_setup(C8, 2, false);
-                }
-            }
+    fn process(&mut self, nibble: u8, handler: &mut dyn FnMut(Result)) {
+        match self.state {
+            Unsynced => return,
+            OpCode => self.process_opcode(nibble, handler),
+            Payload => self.process_payload(nibble, handler),
+            Timestamp => return,
         }
     }
+
+    fn process_opcode(&mut self, nibble: u8, _handler: &mut dyn FnMut(Result)) {
+        match self.span {
+            0 => match nibble {
+                0 => return, // NULL packet.
+                1 => self.packet_setup(M8, 2, false),
+                2 => self.packet_setup(MERR, 2, false),
+                3 => self.packet_setup(C8, 2, false),
+                _ => return,
+            },
+            _ => return,
+        }
+
+        self.span += 1;
+    }
+
+    fn packet_setup(&mut self, op_code: OpCode, payload_sz: u8, has_timestamp: bool) {
+        self.op_code = Some(op_code);
+        self.payload_sz = payload_sz;
+        self.has_timestamp = has_timestamp;
+
+        if payload_sz > 0 {
+            self.to_state(Payload);
+        } else if has_timestamp == true {
+            self.to_state(Timestamp);
+        } else {
+            panic!("Invalid packet_setup");
+        }
+    }
+
+    fn process_payload(&mut self, _nibble: u8, _handler: &mut dyn FnMut(Result)) {}
+}
--- stm-core/tests/stp_decoder_tests.rs
+++ stm-core/tests/stp_decoder_tests.rs
@@ -1,10 +1,49 @@
-use stm_core::stp_decoder::{Error::*, Result, StpDecoder};
+use stm_core::stp_decoder::{Error::*, Packet, PacketDetails, Result, StpDecoder};
 
+// Unsynced:
 #[test]
-fn basic_fsync() {
+fn basic_unsynced() {
     let mut results = Vec::<Result>::new();
     let mut decoder = StpDecoder::new();
-    let stream = [0; 32];
+    let stream = [1; 32];
 
     decoder.decode_bytes(&stream, |r| results.push(r));
+    assert_eq!(results.len(), 0);
+}
+
+#[test]
+fn basic_async() {
+    let mut results = Vec::<Result>::new();
+    let mut exp = Vec::<Result>::new();
+    let mut decoder = StpDecoder::new();
+    let mut stream = [0xFF; 11];
+    stream[10] = 0x0F;
+
+    decoder.decode_bytes(&stream, |r| results.push(r));
+
+    exp.push(Ok(Packet {
+        start: 0,
+        span: 22,
+        details: PacketDetails::Async,
+    }));
+
+    assert_eq!(results, exp);
+}
+
+#[test]
+fn invalid_async() {
+    let mut results = Vec::<Result>::new();
+    let mut exp = Vec::<Result>::new();
+    let mut decoder = StpDecoder::new();
+    let mut stream = [0xFF; 11];
+    stream[10] = 0x1F;
+
+    decoder.decode_bytes(&stream, |r| results.push(r));
+
+    exp.push(Err(InvalidAsync {
+        start: 0,
+        value: 0x01,
+    }));
+
+    assert_eq!(results, exp);
 }
