e5ee766f419a5b830750118865e7902e79bfe829
Nathan Miller <Nathan.Miller@wdc.com>
2020-05-12 22:44:05 -0500

    Initial CLI

--- stp-cli/src/main.rs
+++ stp-cli/src/main.rs
@@ -1,14 +1,35 @@
 #[macro_use]
 extern crate clap;
 
+use clap::ArgMatches;
+
 fn main() {
-    let matches = clap_app!(stp =>
+    let app_m = clap_app!(stp =>
         (version: "0.1")
         (author: "Nathan M. <nathanm2@gmail.com>")
-        (about: "A tool for working with System Trace Protocol (STP) data.")
+        (about: "A tool for working with System Trace Protocol (STP) data")
         (@subcommand nibbles =>
             (about: "Displays the nibble stream")
+            (@arg FILE: "STP file")
+        )
+        (@subcommand packets =>
+            (about: "Displays the STP packets")
+            (@arg FILE: "STP file")
         )
     )
     .get_matches();
+
+    match app_m.subcommand() {
+        ("nibbles", Some(sub_m)) => nibbles(sub_m),
+        ("packets", Some(sub_m)) => packets(sub_m),
+        _ => {}
+    }
+}
+
+fn nibbles(_sub_m: &ArgMatches) {
+    print!("Inside nibbles\n");
+}
+
+fn packets(_sub_m: &ArgMatches) {
+    print!("Inside packets\n");
 }
