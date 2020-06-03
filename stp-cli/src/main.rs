f5c113f6733a8331add3a94b90feda0fbfa9012c
Nathan Miller <Nathan.Miller@wdc.com>
2020-06-02 19:55:30 -0500

    WIP: CLI

--- stp-cli/src/main.rs
+++ stp-cli/src/main.rs
@@ -2,17 +2,21 @@
 extern crate clap;
 
 use clap::ArgMatches;
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, ErrorKind};
 use std::result;
+use stp_core::frame_decoder::{self, decode_frames};
 
 const PROG_NAME: &str = crate_name!();
 
-type Result = result::Result<(), String>;
+struct CliError(String);
+
+type Result = result::Result<(), CliError>;
 
 fn main() {
-    if let Err(message) = run() {
-        eprintln!("{}: {}", PROG_NAME, message);
+    if let Err(cli_error) = run() {
+        eprintln!("{}: {}", PROG_NAME, cli_error.0);
         std::process::exit(1);
     }
 }
@@ -43,11 +47,11 @@ fn run() -> Result {
     }
 }
 
-fn get_input(sub_m: &ArgMatches) -> result::Result<Box<dyn Read>, String> {
+fn get_input(sub_m: &ArgMatches) -> result::Result<Box<dyn Read>, CliError> {
     match sub_m.value_of("FILE") {
         Some(path) => match File::open(path) {
             Ok(f) => Ok(Box::new(f)),
-            Err(e) => Err(format!("{}: {}", e, path)),
+            Err(e) => Err(CliError(format!("{}: {}", e, path))),
         },
         None => Ok(Box::new(io::stdin())),
     }
@@ -55,23 +59,82 @@ fn get_input(sub_m: &ArgMatches) -> result::Result<Box<dyn Read>, String> {
 
 const BUF_SIZE: usize = 4 * 1024;
 
+impl std::convert::From<frame_decoder::Error> for CliError {
+    fn from(err: frame_decoder::Error) -> CliError {
+        CliError(format!("{}", err))
+    }
+}
+
 fn streams(_app_m: &ArgMatches, sub_m: &ArgMatches) -> Result {
     let mut input = get_input(sub_m)?;
     let mut buf = [0; BUF_SIZE];
     let mut total = 0;
+    let mut stream_id = None;
+    let mut display = StreamDisplay::new();
 
     loop {
         let len = match input.read(&mut buf) {
             Ok(0) => return Ok(()),
             Ok(len) => len,
             Err(e) if e.kind() == ErrorKind::Interrupted => continue,
-            Err(e) => return Err(format!("{}", e)),
+            Err(e) => return Err(CliError(format!("{}", e))),
         };
+        stream_id = decode_frames(
+            &buf[..len],
+            stream_id,
+            |id, data| display.display(id, data),
+            |mut e| {
+                e.offset += total;
+                Err(e)
+            },
+        )?;
         total += len;
     }
 }
 
+struct StreamDisplay {
+    offsets: HashMap<Option<u8>, usize>,
+    cur_id: Option<u8>,
+    cur_offset: usize,
+    col: usize,
+}
+
+impl StreamDisplay {
+    fn new() -> StreamDisplay {
+        StreamDisplay {
+            offsets: HashMap::new(),
+            cur_id: Some(0xFF), // Intentionally set to an invalid Stream ID.
+            cur_offset: 0,
+            col: 0,
+        }
+    }
+
+    fn display(&mut self, id: Option<u8>, data: u8) {
+        if id != self.cur_id {
+            self.offsets.insert(self.cur_id, self.cur_offset);
+            self.cur_offset = *self.offsets.entry(id).or_insert(0);
+            self.col = 0;
+            self.cur_id = id;
+            match id {
+                None => print!("\n\nStream None:"),
+                Some(id) => print!("\n\nStream {:#X}:", id),
+            }
+        }
+
+        if self.col % 16 == 0 {
+            print!("\n{:012X} |", self.cur_offset * 2);
+            self.col = 0;
+        } else if self.col == 8 {
+            print!(" ");
+        }
+        print!(" {:x} {:x}", data & 0xF, data >> 4);
+
+        self.col += 1;
+        self.cur_offset += 1;
+    }
+}
+
 fn packets(_app_m: &ArgMatches, sub_m: &ArgMatches) -> Result {
-    let mut input = get_input(sub_m)?;
+    let mut _input = get_input(sub_m)?;
     Ok(())
 }
