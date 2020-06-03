4cd1a51b56a01b695ed65e0927339e0f317d2861
Nathan Miller <Nathan.Miller@wdc.com>
2020-06-02 23:25:44 -0500

    WIP: working toward a single closure

--- stp-core/src/frame_decoder.rs
+++ stp-core/src/frame_decoder.rs
@@ -7,9 +7,21 @@ pub enum ErrorReason {
     InvalidStreamId(u8),
     InvalidAuxByte(u8),
     PartialFrame(usize),
+    Stop,
 }
 
 use self::ErrorReason::*;
+
+impl fmt::Display for ErrorReason {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            InvalidStreamId(id) => write!(f, "invalid stream id: {:#x}", id),
+            InvalidAuxByte(byte) => write!(f, "invalid aux byte: {:#x}", byte),
+            PartialFrame(size) => write!(f, "truncated frame: {} bytes", size),
+            Stop => write!(f, "stopped"),
+        }
+    }
+}
 
 #[derive(Debug, PartialEq)]
 pub struct Error {
@@ -19,11 +31,7 @@ pub struct Error {
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self.reason {
-            InvalidStreamId(id) => write!(f, "invalid stream id: {:#x} ({})", id, self.offset),
-            InvalidAuxByte(byte) => write!(f, "invalid aux byte: {:#x} ({})", byte, self.offset),
-            PartialFrame(size) => write!(f, "partial frame: {} bytes ({})", size, self.offset),
-        }
+        write!(f, "{} ({})", self.reason, self.offset)
     }
 }
 
@@ -34,39 +42,31 @@ pub type Result = result::Result<Option<u8>, Error>;
 /// # Arguments
 ///
 ///  * `frames` - A stream of bytes representing contiguous 16 byte frames.
-///  * `stream` - The starting stream ID.
-///  * `data` - Data handler closure. This closure takes two arguments: a stream ID and a
-///             single byte of data associated with that stream ID.
-///  * `error` - Error handler.  This is closure that takes an Error and returns a Result.  If the
-///              Result is an Error, decoding will be halted, otherwise decoding will continue.
-pub fn decode_frames<D, E>(
-    frames: &[u8],
-    stream_id: Option<u8>,
-    mut data: D,
-    mut error: E,
-) -> Result
+///  * `stream_id` - The starting stream ID.
+pub fn decode_frames<H>(frames: &[u8], stream_id: Option<u8>, mut handler: H) -> Result
 where
-    D: FnMut(Option<u8>, u8),
-    E: FnMut(Error) -> result::Result<(), Error>,
+    H: FnMut(result::Result<(Option<u8>, u8), Error>) -> result::Result<(), Error>,
 {
     let mut id = stream_id;
     let mut offset = 0;
     let mut iter = frames.chunks_exact(16);
 
     for frame in &mut iter {
-        id = decode_frame(frame.try_into().unwrap(), id, &mut data, |mut e| {
-            e.offset += offset;
-            error(e)
+        id = decode_frame(frame.try_into().unwrap(), id, |mut r| {
+            if let Err(ref mut e) = r {
+                e.offset += offset;
+            }
+            handler(r)
         })?;
         offset += 16;
     }
 
     let remainder = iter.remainder().len();
     if remainder > 0 {
-        error(Error {
+        handler(Err(Error {
             offset: offset,
             reason: PartialFrame(remainder),
-        })?;
+        }))?;
     }
 
     Ok(id)
@@ -78,28 +78,19 @@ where
 ///
 ///  * `frame` - The frame of data to be decoded.
 ///  * `stream` - The starting stream ID.
-///  * `data` - Stream data handler.  This is a closure that takes two arguments: a stream ID and a
-///             single byte of data.
-///  * `error` - Error handler.  This is closure that takes an Error.  If it returns an Error
-///             decoding will be halted.  Otherwise decoding will continue.
-///
-pub fn decode_frame<D, E>(
-    frame: &[u8; 16],
-    stream_id: Option<u8>,
-    mut data: D,
-    mut error: E,
-) -> Result
+pub fn decode_frame<H>(frame: &[u8; 16], stream_id: Option<u8>, mut handler: H) -> Result
 where
-    D: FnMut(Option<u8>, u8),
-    E: FnMut(Error) -> result::Result<(), Error>,
+    H: FnMut(result::Result<(Option<u8>, u8), Error>) -> result::Result<(), Error>,
 {
-    let aux_byte = frame[15];
+    let mut aux_byte = frame[15];
 
+    // If byte 14 contains a frame id, it's aux bit should be zero per the spec:
     if aux_byte & 0x80 == 0x80 && frame[14] & 0x01 == 0x01 {
-        error(Error {
+        handler(Err(Error {
             offset: 15,
             reason: InvalidAuxByte(aux_byte),
-        })?;
+        }))?;
+        aux_byte = aux_byte & 0x7F;
     }
 
     let mut cur_stream = stream_id;
@@ -111,10 +102,10 @@ where
             let aux_bit = (aux_byte >> (i / 2)) & 0x01;
             if byte & 0x01 == 1 {
                 if *byte == 0xFF {
-                    error(Error {
+                    handler(Err(Error {
                         offset: i,
                         reason: InvalidStreamId(0x7F),
-                    })?;
+                    }))?;
                 }
                 // Id Change.
                 if aux_bit == 1 {
@@ -125,11 +116,11 @@ where
                     cur_stream = Some(byte >> 1);
                 }
             } else {
-                data(cur_stream, byte | aux_bit);
+                handler(Ok((cur_stream, byte | aux_bit)))?;
             }
         } else {
             // Odd byte: Data only.
-            data(cur_stream, *byte);
+            handler(Ok((cur_stream, *byte)))?;
             if let Some(_) = next_stream {
                 cur_stream = next_stream;
                 next_stream = None;
@@ -137,8 +128,5 @@ where
         }
     }
 
-    match next_stream {
-        Some(_) => Ok(next_stream),
-        None => Ok(cur_stream),
-    }
+    Ok(cur_stream)
 }
