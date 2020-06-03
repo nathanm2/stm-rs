4bb8c6d2edd7dba19398c04bf0d4066cfd302cc7
Nathan Miller <Nathan.Miller@wdc.com>
2020-06-02 22:13:30 -0500

    Working CLI

--- stp-cli/src/packets.rs
+++
@@ -1,5 +0,0 @@
-use clap::ArgMatches;
-
-pub fn packets(_app_m: &ArgMatches, _sub_m: &ArgMatches) {
-    print!("Inside packets\n");
-}
--- stp-cli/src/streams.rs
+++
@@ -1,5 +0,0 @@
-use clap::ArgMatches;
-
-pub fn streams(_app_m: &ArgMatches, _sub_m: &ArgMatches) {
-    print!("Inside streams\n");
-}
