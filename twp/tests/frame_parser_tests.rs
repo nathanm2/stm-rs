4ced04b828085f673a688cab110c931e72a147c9
Nathan Miller <Nathan.Miller@wdc.com>
2021-05-03 09:09:54 -0500

    WIP: updating tests

--- twp/tests/frame_parser_tests.rs
+++ twp/tests/frame_parser_tests.rs
@@ -4,15 +4,18 @@ use twp::frame_parser::*;
 
 type DataMap = HashMap<Option<StreamId>, Vec<u8>>;
 
-fn record_parse_frame(
+// Utility function that populates a map from FrameBytes and optionally records the errors
+// encountered.
+fn record_parse_frames(
     frames: &[u8],
     data_map: &mut DataMap,
-    mut errors: Option<&mut Vec<Error>>,
+    errors: Option<&mut Vec<Error>>,
 ) -> Result<Option<StreamId>> {
-    parse_frames(&frames, None, |r, o| match r {
+    let mut tmp_errors = errors;
+    parse_frames(frames, None, |r, o| match r {
         Ok(FrameByte { data, id }) => {
             data_map
-                .entry(id.into())
+                .entry(id)
                 .and_modify(|v| v.push(data))
                 .or_insert(vec![data]);
             Ok(())
@@ -22,7 +25,7 @@ fn record_parse_frame(
                 kind: kind,
                 offset: o,
             };
-            match &mut errors {
+            match tmp_errors.as_mut() {
                 Some(v) => {
                     v.push(e);
                     Ok(())
@@ -33,22 +36,88 @@ fn record_parse_frame(
     })
 }
 
-// Feed a partial frame into the decoder:
+// Pass one complete frame and one partial frame to the parser:
 #[test]
 fn partial_frame() {
-    let frames = [0; 31];
+    let frames = [0; 30];
     let mut data_map = DataMap::new();
-    let result = record_parse_frame(&frames, &mut data_map, None);
-    let mut expected_data = DataMap::new();
+    let result = record_parse_frames(&frames, &mut data_map, None);
 
     assert_eq!(
         result,
         Err(Error {
-            kind: ErrorKind::PartialFrame(15),
+            kind: ErrorKind::PartialFrame(14),
             offset: 16,
         })
     );
 
+    let mut expected_data = DataMap::new();
     expected_data.insert(None, vec![0; 15]);
     assert_eq!(data_map, expected_data);
+}
+
+// Parse a frame with an invalid ID:
+#[test]
+fn bad_id() {
+    let mut frames = [0; 32];
+
+    frames[0] = 0x03;
+    frames[16] = 0xFF;
+
+    let mut data_map = DataMap::new();
+    let result = record_parse_frames(&frames, &mut data_map, None);
+
+    assert_eq!(
+        result,
+        Err(Error {
+            offset: 16,
+            kind: ErrorKind::InvalidStreamId(0x7F)
+        })
+    );
+
+    let mut expected = HashMap::new();
+    expected.insert(Some(StreamId::from(1)), vec![0; 14]);
+
+    assert_eq!(data_map, expected);
+}
+
+// Parse a frame with an invalid ID: (continue on)
+#[test]
+fn bad_id_continue() {
+    let mut frames = [0; 32];
+
+    frames[0] = 0x03;
+    frames[16] = 0xFF;
+
+    let mut data_map = DataMap::new();
+    let mut errors = Vec::new();
+    let result = record_parse_frames(&frames, &mut data_map, Some(&mut errors));
+    assert_eq!(result, Ok(Some(StreamId::from(127))));
+
+    let mut expected = HashMap::new();
+    expected.insert(Some(StreamId::from(1)), vec![0; 14]);
+    expected.insert(Some(StreamId::from(0x7F)), vec![0; 14]);
+
+    assert_eq!(data_map, expected);
+
+    let expected_errors = vec![Error {
+        offset: 16,
+        kind: ErrorKind::InvalidStreamId(0x7F),
+    }];
+    assert_eq!(errors, expected_errors);
+}
+
+// Two frames of data with an unknown stream id:
+#[test]
+fn unknown_id() {
+    let frames = [0; 32];
+
+    let mut data_map = DataMap::new();
+    let result = record_parse_frames(&frames, &mut data_map, None);
+    assert_eq!(result, Ok(None));
+
+    let mut expected = HashMap::new();
+    expected.insert(None, vec![0; 30]);
+
+    assert_eq!(data_map, expected);
 }
